b4130eb275362ab046875a1cd3a16b30cacc446a
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-28 16:17:54 -0800

    removed custom box section hook

--- inc/theme-actions.php
+++ inc/theme-actions.php
@@ -25,9 +25,6 @@ add_action( 'ifeature_header_contact_area', 'ifeature_header_contact_area_conten
 remove_action( 'chimps_head_tag', 'chimps_link_rel' );
 add_action( 'chimps_head_tag', 'ifeature_link_rel' );
 
-remove_action( 'chimps_box_section', 'chimps_box_section_content' );
-add_action( 'chimps_box_section', 'ifeature_box_section_content' );
-
 remove_action( 'chimps_navigation', 'chimps_nav' );
 add_action( 'chimps_navigation', 'ifeature_nav' );
 
@@ -136,46 +133,6 @@ function ifeature_header_contact_area_content() {
 }
 
 /**
-* Sets up the Box Section wigetized area
-*
-* @since 3.0.5
-*/
-function ifeature_box_section_content() { 
-	global $post; //call globals
-	
-	$enableboxes = get_post_meta($post->ID, 'enable_box_section' , true);
-	$root = get_template_directory_uri(); ?>
-	
-
-	<div id="box_container" class="container_12"> <!--box container-->
-		<div class="grid_12">
-		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Box Left") ) : ?>
-			<div class="box1">
-				<h2 class="box-widget-title">iFeature Pro Slider</h2>
-					<img src="<?php echo $root ; ?>/images/icons/slidericon.png" height="100" alt="slider" class="aligncenter" />
-					<p>The iFeature Pro Slider includes auto-image resizing, new transitions, thumbnails, custom categories, improved captions, and the ability to have a slider on every page.</p>
-			</div><!--end box1-->
-			<?php endif; ?>
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Box Middle") ) : ?>
-			<div class="box2">
-				<h2 class="box-widget-title">New Design</h2>
-					<img src="<?php echo $root ; ?>/images/icons/blueprint.png" height="100" alt="blueprint" class="aligncenter" />
-					<p>With <a href="http://cyberchimps.com/ifeaturepro/">iFeature Pro</a> we’ve done the design work for you, all you need to do is pick a color scheme, select your options, and add your content.</p>
-			</div><!--end box2-->
-			<?php endif; ?>
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Box Right") ) : ?>
-			<div class="box3">
-				<h2 class="box-widget-title">Excellent Support</h2>
-				<img src="<?php echo $root ; ?>/images/icons/docs.png" height="100" alt="docs" class="aligncenter" />
-				<p>We designed iFeature Pro to be as easy to design with as possible, if you do run into trouble we provide a <a href="http://cyberchimps.com/forum">support forum</a>, and <a href="http://www.cyberchimps.com/ifeaturepro/docs/">precise documentation</a>.</p>
-			</div><!--end box3-->
-		<?php endif; ?>
-</div>
-	</div><!--end box_container--> <div class='clear'>&nbsp;</div><?php
-}
-
-
-/**
 * Sets the header link rel attributes
 *
 * @since 3.0.2
@@ -221,7 +178,7 @@ function ifeature_link_rel() {
 /**
 * Sets up the Facebook Like and Google Plus One area
 *
-* @since 3.0.5
+* @since 3.1
 */
 function ifeature_fb_like_plus_one_content() {
 	global $options, $themeslug; ?>
@@ -243,7 +200,7 @@ function ifeature_fb_like_plus_one_content() {
 /**
 * Sets up the HTML for the post share section
 *
-* @since 3.0.5
+* @since 3.1
 */
 function ifeature_post_bar_content() { 
 	global $options, $themeslug; 
