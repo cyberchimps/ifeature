0d67de9a6b98ddaacfae4d717218adcb8b8dadf3
Tyler Cunningham <tyler@cyberchimps.com>
2011-12-28 16:10:39 -0800

    Finally a unified comment system

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@
 	Version 3.0
 */
 
-
-/* Define global variables. */	
-
+/**
+* Define the theme globals.
+*/ 
 	$themename = 'ifeature';
 	$themenamefull = 'iFeature';
 	$themeslug = 'if';
@@ -19,14 +19,15 @@
 	$pagedocs = 'http://cyberchimps.com/question/using-the-ifeature-free-3-page-options/';
 	$sliderdocs = 'http://cyberchimps.com/question/using-the-ifeature-3-slider/';
 	
-/* End global variables. */	
-
-//Redirect to theme options after activation
+/**
+* Redirect to the theme options panel upon activation.
+*/ 
 if ( is_admin() && isset($_GET['activated'] ) && $pagenow ==	"themes.php" )
 	wp_redirect( 'themes.php?page=ifeature' );
 	
-//Gallery options 
-
+/**
+* Show a single gallery thumb and list the number of photos in the gallery.
+*/ 
 function if_custom_gallery_post_format( $content ) {
 global $options, $themeslug, $post;
 $root = get_template_directory_uri(); 
@@ -79,11 +80,11 @@ ob_start();
 	
 	return $content;
 }
-
 add_filter('chimps_post_formats_gallery_content', 'if_custom_gallery_post_format' ); 
 
-/* Begin custom excerpt functions. */	
-
+/**
+* Custom post excerpts based on theme options.
+*/ 
 function if_new_excerpt_more($more) {
 
 	global $themename, $themeslug, $options;
@@ -117,107 +118,102 @@ function if_new_excerpt_length($length) {
 }
 add_filter('excerpt_length', 'if_new_excerpt_length');
 
-/* End excerpt functions. */
-
-/* Add auto-feed links support. */	
+/**
+* Auto-feed links.
+*/ 
 add_theme_support('automatic-feed-links');
 	
-/* Add post-thumb support. */
-
+/**
+* Featured image options.
+*/ 
 function if_init_featured_image() {	
-if ( function_exists( 'add_theme_support' ) ) {
-
- global $themename, $themeslug, $options;
+	if ( function_exists( 'add_theme_support' ) ) {
+ 		global $themename, $themeslug, $options;
 	
-	if ($options->get($themeslug.'_featured_image_height') == '') {
+		if ($options->get($themeslug.'_featured_image_height') == '') {
 			$featureheight = '100';
-	}		
-	else {
-		$featureheight = $options->get($themeslug.'_featured_image_height'); 
+		}		
+		else {
+			$featureheight = $options->get($themeslug.'_featured_image_height'); 
 		
-	}
-	if ($options->get($themeslug.'_featured_image_width') == "") {
+		}
+		if ($options->get($themeslug.'_featured_image_width') == "") {
 			$featurewidth = '100';
-	}		
-	else {
-		$featurewidth = $options->get($themeslug.'_featured_image_width'); 
-	} 
+		}		
+		else {
+			$featurewidth = $options->get($themeslug.'_featured_image_width'); 
+		} 
 	 
 	set_post_thumbnail_size( $featureheight, $featurewidth, true );
-}	
+	}	
 }
 add_action( 'init', 'if_init_featured_image', 11);	
 
-// Featured image support.
+/**
+* Support featured images.
+*/ 
 add_theme_support( 'post-thumbnails' );
 
-// This theme styles the visual editor with editor-style.css to match the theme style.
+/**
+* Editor style.
+*/ 
 add_editor_style();
 
 /**
-* Attach CSS3PIE behavior to elements
-* Add elements here that need PIE applied
+* PIE support.
 */   
 function if_render_ie_pie() { ?>
 	
 	<style type="text/css" media="screen">
 		#wrapper input, textarea, input[type=submit], input[type=reset], #imenu, .searchform, .post_container, .postformats, .postbar, .post-edit-link, .widget-container, .widget-title, .footer-widget-title, .comments_container, ol.commentlist li.even, ol.commentlist li.odd, .slider_nav, #twitterbar, ul.metabox-tabs li, .tab-content, .list_item, .section-info, #of_container #header, .menu ul li a, .submit input, #of_container textarea, #of_container input, #of_container select, #of_container .screenshot img, #of_container .of_admin_bar, #of_container .subsection > h3, .subsection, #of_container #content .outersection .section
   		
-  			{
-  				behavior: url('<?php echo get_template_directory_uri(); ?>/core/library/pie/PIE.htc');
-			}
+  		{behavior: url('<?php echo get_template_directory_uri(); ?>/core/library/pie/PIE.htc');}
+  		
 	</style>
 <?php
 }
 
 add_action('wp_head', 'if_render_ie_pie', 8);
 	
-// Nivo Slider 
-
+/**
+* Call script for Nivo Slider.
+*/ 
 function if_nivoslider(){
 	 
 	$path =  get_template_directory_uri() ."/core/library/ns";
-
-	$script = "
-		
-		<script type=\"text/javascript\" src=\"".$path."/jquery.nivo.slider.js\"></script>
-		";
+	$script = "<script type=\"text/javascript\" src=\"".$path."/jquery.nivo.slider.js\"></script>";
 	
 	echo $script;
 }
 add_action('wp_head', 'if_nivoslider');
 
-// + 1 Button 
-
+/**
+* Script for +1 button.
+*/ 
 function if_plusone(){
 	
 	$path =  get_template_directory_uri() ."/core/library/js";
-
-	$script = "
-		
-		<script type=\"text/javascript\" src=\"".$path."/plusone.js\"></script>
-		";
+	$script = "<script type=\"text/javascript\" src=\"".$path."/plusone.js\"></script>";
 	
 	echo $script;
 }
 add_action('wp_head', 'if_plusone');
 
-// Menu JS
-
+/**
+* Menu JS.
+*/ 
 function if_menu_script(){
 	
 	$path =  get_template_directory_uri() ."/core/library/js";
-
-	$script = "
-		
-		<script type=\"text/javascript\" src=\"".$path."/menu.js\"></script>
-		";
+	$script = "<script type=\"text/javascript\" src=\"".$path."/menu.js\"></script>";
 	
 	echo $script;
 }
 add_action('wp_footer', 'if_menu_script');
 	
-// Load jQuery
+/**
+* Load jQuery.
+*/ 
 function if_jquery() {
 	if ( !is_admin() ) {
 	   wp_enqueue_script('jquery');
@@ -225,17 +221,19 @@ function if_jquery() {
 }
 add_action('wp_enqueue_scripts', 'if_jquery');	
 	
-	// Register menu names
-	
-	function if_register_menus() {
+/**
+* Register custom menus.
+*/ 
+function if_register_menus() {
 	register_nav_menus(
 	array( 'header-menu' => 'Header Menu', 'footer-menu' => 'Footer Menu' )
   );
 }
 	add_action( 'init', 'if_register_menus' );
-	
 
-
+/**
+* Initialize the widgets. 
+*/ 
 function ifp_widgets_init() {
     register_sidebar(array(
     	'name' => 'Sidebar Widgets',
@@ -258,32 +256,37 @@ function ifp_widgets_init() {
 }
 add_action ('widgets_init', 'ifp_widgets_init');
 
-//Add link to theme settings in Admin bar
-
+/**
+* Link to theme options in admin bar.
+*/ 
 function if_admin_link() {
-
 	global $wp_admin_bar;
 
 	$wp_admin_bar->add_menu( array( 'id' => 'iFeature', 'title' => 'iFeature Options', 'href' => admin_url('themes.php?page=ifeature')  ) ); 
-  
 }
 add_action( 'admin_bar_menu', 'if_admin_link', 113 );
 
-//Set content width
-
+/**
+* Content width.
+*/ 
 if ( ! isset( $content_width ) ) $content_width = 608;
 
-//hooks
-
+/**
+* Initialize the CyberChimps Core Framework.
+*/ 
 require_once ( get_template_directory() . '/core/core-init.php' );
-
 do_action('chimps_init');
 
-// Call additional template files
+/**
+* Call additional files.
+*/ 
 require_once ( get_template_directory() . '/inc/classy-options-init.php' );
 require_once ( get_template_directory() . '/inc/options-functions.php' );
 require_once ( get_template_directory() . '/inc/meta-box.php' );	
 require_once ( get_template_directory() . '/inc/theme-hooks.php' ); 
 require_once ( get_template_directory() . '/inc/theme-actions.php' ); 
 
+/**
+* End of file.
+*/ 
 ?>
