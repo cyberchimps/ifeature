239df966f648f7fdd8217afb2a872c28a52cc08e
Tyler Cunningham <tyler@cyberchimps.com>
2012-04-16 12:02:42 -0700

    removed unused translation functions, fixed link to docs

--- core/classy-options/classy-options-framework/classy-options.php
+++ core/classy-options/classy-options-framework/classy-options.php
@@ -101,13 +101,13 @@ class ClassyOptions {
 					</p>
 				<div class="menu">
 					<ul class="buttons">
-						<li><a href="http://cyberchimps.com/store/" target="_blank"><?php printf( __( 'CyberChimps Store', 'ifeature' )); ?></a></li>
-						<li><a href="http://cyberchimps.com/support" target="_blank"><?php printf( __( 'Support', 'ifeature' )); ?></a></li>
-						<li><a href="http://cyberchimps.com/ifeaturepro/docs/" target="_blank"> <?php printf( __( 'Documentation', 'ifeature' )); ?></a></li>
-						<li><a href="http://cyberchimps.com/forum/" target="_blank"><?php printf( __( 'Forum', 'ifeature' )); ?></a></li>
-						<li><a href="http://twitter.com/#!/cyberchimps" target="_blank"><?php printf( __( 'Twitter', 'ifeature' ));?></a></li>
-						<li><a href="http://facebook.com/cyberchimps/" target="_blank"><?php printf( __( 'Facebook', 'ifeature' ));?></a></li>
-						<li><a href="http://cyberchimpspro.com/" target="_blank"><?php printf( __( 'CyberChimps Pro', 'ifeature' )); ?></a></li>
+						<li><a href="http://cyberchimps.com/" target="_blank">CyberChimps</a></li>
+						<li><a href="http://cyberchimps.com/store/" target="_blank">Store</a></li>
+						<li><a href="http://cyberchimps.com/support" target="_blank">Support</a></li>
+						<li><a href="http://cyberchimps.com/ifeature-free/docs/" target="_blank">Documentation</a></li>
+						<li><a href="http://cyberchimps.com/forum/" target="_blank">Forum</a></li>
+						<li><a href="http://twitter.com/#!/cyberchimps" target="_blank">Twitter</a></li>
+						<li><a href="http://facebook.com/cyberchimps/" target="_blank">Facebook</a></li>
 					</ul>
 				</div>
 
