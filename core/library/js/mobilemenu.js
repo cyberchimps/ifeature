ae209b24ee26c609cb0385dc404dc16e567af854
Tyler Cunningham <Sanchez@Tylers-MacBook-Air.local>
2012-04-30 17:36:49 -0700

    needed 20 more pixels

--- core/library/js/mobilemenu.js
+++ core/library/js/mobilemenu.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($){
     
     //plugin's default options
     var settings = {
-      switchWidth: 480,
+      switchWidth: 500,
       topOptionText: 'Select a page',
       indentString: '&nbsp;&nbsp;&nbsp;'
     };
