7df73354823e4742b5700ea465e43cabd3040b1e
Tyler Cunningham <Sanchez@Tylers-MacBook-Air.local>
2012-05-22 12:04:46 -0700

    removed redirect code that did not work

--- functions.php
+++ functions.php
@@ -40,8 +40,7 @@ add_action( 'after_setup_theme', 'if_theme_setup' );
 /**
 * Redirect user to theme options page after activation.
 */ 
-if ( is_admin() && ( ( isset($_GET['activated'] ) && $pagenow == 'themes.php') || $pagenow == 'update-core.php?action=do-theme-upgrade') ) {
-
+if ( is_admin() && isset($_GET['activated'] ) && $pagenow =="themes.php" ) {
 	wp_redirect( 'themes.php?page=ifeature' );
 }
 
