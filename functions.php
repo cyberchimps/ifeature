97d359cb34a84b48b407b133e2a4f73e6cb3d725
sagarvadn <sagar.vadnere@hbwsl.com>
2017-06-16 12:15:41 +0530

    Changed changelog

--- functions.php
+++ functions.php
@@ -643,7 +643,7 @@ function ifeature_typography_h3()
 	return $default;
 }
 
-function ifeature_customize_register( $wp_customize ) {
+function ifeature_customize_edit_links( $wp_customize ) {
    
    $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
    $wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
@@ -659,33 +659,27 @@ function ifeature_customize_register( $wp_customize ) {
 	) );
 
 	$wp_customize->selective_refresh->add_partial( 'cyberchimps_options[custom_logo]', array(
-		'selector' => '#logo',
-		'render_callback' => 'ifeature_customize_partial_blogname',
+		'selector' => '#logo'
 	) );
 
 	$wp_customize->selective_refresh->add_partial( 'cyberchimps_options[theme_backgrounds]', array(
-		'selector' => '#social',
-		'render_callback' => 'ifeature_customize_partial_blogname',
+		'selector' => '#social'
 	) );
 
 	$wp_customize->selective_refresh->add_partial( 'cyberchimps_options[searchbar]', array(
-		'selector' => '#searchform',
-		'render_callback' => 'ifeature_customize_partial_blogname',
+		'selector' => '#searchform'
 	) );
 
 	$wp_customize->selective_refresh->add_partial( 'cyberchimps_options[footer_show_toggle]', array(
-		'selector' => '#footer_wrapper',
-		'render_callback' => 'ifeature_customize_partial_blogname',
+		'selector' => '#footer_wrapper'
 	) );
 
 	$wp_customize->selective_refresh->add_partial( 'cyberchimps_options[footer_copyright_text]', array(
-		'selector' => '#copyright',
-		'render_callback' => 'ifeature_customize_partial_blogname',
+		'selector' => '#copyright'
 	) );
 
 	$wp_customize->selective_refresh->add_partial( 'nav_menu_locations[primary]', array(
-		'selector' => '#navigation',
-		'render_callback' => 'ifeature_customize_partial_blogname',
+		'selector' => '#navigation'
 	) );
 
 }
@@ -697,4 +691,4 @@ function ifeature_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
-add_action( 'customize_register', 'ifeature_customize_register' );
+add_action( 'customize_register', 'ifeature_customize_edit_links' );
